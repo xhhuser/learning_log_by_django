38a6b2c1f20c81d34e0bd3e85f79c582e597b6d8
“HowardXugit config --global user.name “HowardXu <xhh88245056@126.com>
2020-11-18 15:39:05 +0800

    补充setting中的os包

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 """存放自己所有的应用程序"""
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
